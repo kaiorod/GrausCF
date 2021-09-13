cfad40a3716fc812896cabe912da7aab0e9e584b
kaiorod <caiorodrigo2775@gmail.com>
2021-09-12 22:38:19 -0300

    Finaliza GrausCF

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, convf;
+
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Digite o valor em ºC : ");
+            c = double.Parse(Console.ReadLine());
+
+            convf = c * 1.8 + 32;
+            
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"\n{convf} ºF ");
+            Console.ResetColor();
+        }
+    }
+}
